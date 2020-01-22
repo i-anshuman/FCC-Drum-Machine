759878a48428b144263309759161ed1f2bf029b7
Anshuman Gupta <anshuman844@gmail.com>
2020-01-22 21:25:03 +0530

    Drum machine v1.0

--- src/drum-machine.js
+++ src/drum-machine.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './drum-machine.scss';
 
 import PadBank from './pad-bank';
+import ControlPanel from './control-panel';
 
 function DrumMachine() {
+  const [bank, switchBank] = useState('bankOne');
+  const [power, switchPower] = useState(true);
+  const [volume, tuneVolume] = useState(0.3);
+  const [pad, handlePad] = useState(false);
+
+  const handleSettings = (settings) => {
+    switchBank(settings.bank);
+    switchPower(settings.power);
+    tuneVolume(settings.volume);
+  }
+
   return (
     <main className="wrapper">
       <div className="drum-machine" id="drum-machine">
         <div className="drum-machine__logo">
           <span>FCC&nbsp;<i className="fa fa-free-code-camp"></i></span>
         </div>
-        <PadBank bank='bankOne'/>
+        <PadBank bank={bank}
+          power={power}
+          volume={volume}
+          padPlayed={(pad) => {
+            handlePad(pad)
+            setTimeout(() => handlePad(false), 5000)
+          }}
+        />
+        <ControlPanel handleControlPanel={(settings) => handleSettings(settings)}
+          padPlayed={pad}
+        />
       </div>
     </main>
   );
