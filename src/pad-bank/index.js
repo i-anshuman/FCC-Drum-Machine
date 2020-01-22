40df242c7459b8715d57e29e8dd38cbd2ba91826
Anshuman Gupta <anshuman844@gmail.com>
2020-01-22 21:23:23 +0530

    Pad bank modified to receive and send props to control pannel.

--- src/pad-bank/index.js
+++ src/pad-bank/index.js
@@ -1,129 +1,22 @@
 import React from 'react';
 import './index.scss';
+import banks from '../banks.js';
 import DrumPad from '../drum-pad';
 
 const PadBank = (props) => {
-  const pads = {
-    bankOne: [
-      {
-        id: 'Heater-1',
-        key: 'Q',
-        keyCode: 81,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
-      },
-      {
-        id: 'Heater-2',
-        key: 'W',
-        keyCode: 87,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-      },
-      {
-        id: 'Heater-3',
-        key: 'E',
-        keyCode: 69,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-      },
-      {
-        id: 'Heater-4',
-        key: 'A',
-        keyCode: 65,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
-      },
-      {
-        id: 'Clap',
-        key: 'S',
-        keyCode: 83,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-      },
-      {
-        id: 'Open-HH',
-        key: 'D',
-        keyCode: 68,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-      },
-      {
-        id: 'Kick-n\'-Hat',
-        key: 'Z',
-        keyCode: 90,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-      },
-      {
-        id: 'Kick',
-        key: 'X',
-        keyCode: 88,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-      },
-      {
-        id: 'Closed-HH',
-        key: 'C',
-        keyCode: 67,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-      }
-    ],
-    bankTwo: [
-      {
-        id: 'Chord-1',
-        key: 'Q',
-        keyCode: 81,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
-      },
-      {
-        id: 'Chord-2',
-        key: 'W',
-        keyCode: 87,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
-      },
-      {
-        id: 'Chord-3',
-        key: 'E',
-        keyCode: 69,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
-      },
-      {
-        id: 'Shaker',
-        key: 'A',
-        keyCode: 65,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
-      },
-      {
-        id: 'Open-HH',
-        key: 'S',
-        keyCode: 83,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
-      },
-      {
-        id: 'Closed-HH',
-        key: 'D',
-        keyCode: 68,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
-      },
-      {
-        id: 'Punchy-Kick',
-        key: 'Z',
-        keyCode: 90,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
-      },
-      {
-        id: 'Side-Stick',
-        key: 'X',
-        keyCode: 88,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
-      },
-      {
-        id: 'Snare',
-        key: 'C',
-        keyCode: 67,
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
-      }
-    ]
-  };
-
   return (
     <div className="pad-bank">
       {
-        pads[props.bank].map((pad, i) => {
+        banks[props.bank].map((pad, i) => {
           return (
-            <DrumPad key={i} data={pad}/>
+            <DrumPad key={i}
+              data={pad}
+              power={props.power}
+              volume={props.volume}
+              handleOnPlay={(status) => {
+                status && props.padPlayed(pad.id)
+              }}
+            />
           )
         })
       }
